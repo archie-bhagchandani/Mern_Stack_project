bedf1fa1cde4f43148de8a73ea5352209e346c57
Archie <archiebhagchandani26@gmail.com>
2026-07-09 12:51:07 +0530

    Remove mam.html from repo, keep locally

--- module/cart.mjs
+++
@@ -1,7 +0,0 @@
-import {products,productsDetails} from "./product.js"
-import a from "./product.js"
-console.log("Cart Page");
-console.log(products)
-productsDetails();
-// productsDisplay();
-a();
